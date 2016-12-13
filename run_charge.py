c5cb4dd17c16a4b4d54def79516f2f3a28a57a35
Nigel W. Moriarty <NWMoriarty@LBL.Gov>
2016-12-12 18:13:50 -0800

    made a single function to call for total charge

--- run_charge.py
+++ run_charge.py
@@ -2,26 +2,33 @@ import os, sys
 
 import iotbx
 
-from run_finalise import get_hetero_charges, default_ion_charges, remove_alt_loc
 from run_finalise import write_hierarchy, get_processed_pdb
-from run_finalise import get_inter_residue_bonds, complete_pdb_hierarchy
-from run_finalise import calculate_pdb_hierarchy_charge
+
+def get_total_charge_from_file_name(pdb_filename,
+                                    hetero_charges=None,
+                                    inter_residue_bonds=None,
+                                   ):
+  from run_finalise import get_hetero_charges, default_ion_charges
+  from run_finalise import get_inter_residue_bonds
+  from run_finalise import calculate_pdb_hierarchy_charge
+  ppf = get_processed_pdb(pdb_filename=pdb_filename)
+  if not hetero_charges:
+    hetero_charges = get_hetero_charges(ppf.all_chain_proxies.pdb_inp)
+    if not hetero_charges:
+      # some defaults
+      hetero_charges = default_ion_charges
+  if not inter_residue_bonds:
+    inter_residue_bonds = get_inter_residue_bonds(ppf)
+  total_charge = calculate_pdb_hierarchy_charge(
+    ppf.all_chain_proxies.pdb_hierarchy,
+    hetero_charges=hetero_charges,
+    inter_residue_bonds=inter_residue_bonds,
+  )
+  return total_charge
 
 def run(pdb_filename):
   print "run",pdb_filename
-  pdb_inp = iotbx.pdb.input(pdb_filename)
-  hetero_charges = get_hetero_charges(pdb_inp)
-  if not hetero_charges:
-    # some defaults
-    hetero_charges = default_ion_charges
-  hierarchy = pdb_inp.construct_hierarchy()
-  #ppf = get_processed_pdb(pdb_filename=pdb_filename)
-  #inter_residue_bonds = get_inter_residue_bonds(ppf)
-  total_charge = calculate_pdb_hierarchy_charge(
-    hierarchy,
-    #hetero_charges=hetero_charges,
-    #inter_residue_bonds=inter_residue_bonds,
-  )
+  total_charge = get_total_charge_from_file_name(pdb_filename)
   print "total_charge",total_charge
 
 if __name__=="__main__":
