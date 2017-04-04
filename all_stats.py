db4d38b5a6ae139023c71ec20bc6921af7a339a1
Pavel <pafonine@lbl.gov>
2017-04-04 13:24:42 +0800

    do not use H in bond rmsd

--- all_stats.py
+++ all_stats.py
@@ -21,7 +21,7 @@ import mmtbx.f_model
 
 mon_lib_srv = monomer_library.server.server()
 ener_lib = monomer_library.server.ener_lib()
-  
+
 def all_single_atom_residues(ph):
   sizes = flex.int()
   for r in ph.residue_groups():
@@ -29,9 +29,19 @@ def all_single_atom_residues(ph):
   fr = sizes.count(sizes[0])*100./sizes.size()
   if(fr>90.): return True
   else: return False
-  
+
+def get_bonds_angles_rmsd(restraints_manager, xrs):
+  hd_sel = xrs.hd_selection()
+  energies_sites = \
+    restraints_manager.select(~hd_sel).energies_sites(
+      sites_cart        = xrs.sites_cart().select(~hd_sel),
+      compute_gradients = False)
+  a_mean = energies_sites.angle_deviations()[2]
+  b_mean = energies_sites.bond_deviations()[2]
+  return a_mean, b_mean
+
 def get_model_stat(file_name):
-  pdb_inp = iotbx.pdb.input(file_name=file_name)  
+  pdb_inp = iotbx.pdb.input(file_name=file_name)
   atoms = pdb_inp.atoms()
   box = uctbx.non_crystallographic_unit_cell_with_the_sites_in_its_center(
     sites_cart   = atoms.extract_xyz(),
@@ -40,7 +50,7 @@ def get_model_stat(file_name):
   ph = pdb_inp.construct_hierarchy()
   if(all_single_atom_residues(ph=ph)): return None
   raw_recs = ph.as_pdb_string(crystal_symmetry=box.crystal_symmetry()).splitlines()
-  #  
+  #
   params = monomer_library.pdb_interpretation.master_params.extract()
   params.clash_guard.nonbonded_distance_threshold=None
   params.disable_uc_volume_vs_n_atoms_check=False
@@ -50,18 +60,16 @@ def get_model_stat(file_name):
     raw_records        = raw_recs,
     params             = params,
     log                = null_out())
+  xrs = processed_pdb_file.xray_structure()
   restraints_manager   = processed_pdb_file.geometry_restraints_manager(
     show_energies      = False,
     plain_pairs_radius = 5.0)
-  #
-  sites_cart = processed_pdb_file.all_chain_proxies.pdb_hierarchy.atoms(
-    ).extract_xyz()
+  a_mean, b_mean = get_bonds_angles_rmsd(
+    restraints_manager = restraints_manager, xrs = xrs)
   energies_sites = \
     restraints_manager.energies_sites(
-      sites_cart        = sites_cart,
+      sites_cart        = xrs.sites_cart(),
       compute_gradients = False)
-  a_mean = energies_sites.angle_deviations()[2]
-  b_mean = energies_sites.bond_deviations()[2]
   nonbonded_distances = energies_sites.nonbonded_distances()
   number_of_worst_clashes = (nonbonded_distances<0.5).count(True)
   #
@@ -89,12 +97,12 @@ def get_model_stat(file_name):
   bs = atoms.extract_b()
   #
   return group_args(
-    b_mean                  = b_mean, 
-    a_mean                  = a_mean, 
-    number_of_worst_clashes = number_of_worst_clashes, 
-    ramachandran_outliers   = ramachandran_outliers, 
-    rotamer_outliers        = rotamer_outliers, 
-    c_beta_dev              = c_beta_dev, 
+    b_mean                  = b_mean,
+    a_mean                  = a_mean,
+    number_of_worst_clashes = number_of_worst_clashes,
+    ramachandran_outliers   = ramachandran_outliers,
+    rotamer_outliers        = rotamer_outliers,
+    c_beta_dev              = c_beta_dev,
     n_cis_proline           = n_cis_proline,
     n_cis_general           = n_cis_general,
     n_twisted_proline       = n_twisted_proline,
@@ -104,7 +112,7 @@ def get_model_stat(file_name):
     mpscore                 = mpscore,
     clsc                    = clsc,
     n_atoms                 = atoms.size())
-            
+
 def get_r(pdb_file, mtz_file):
   miller_arrays = reflection_file_reader.any_reflection_file(file_name =
     mtz_file).as_miller_arrays()
@@ -132,7 +140,7 @@ def get_r(pdb_file, mtz_file):
     xray_structure = xrs)
   fmodel.update_all_scales()
   return fmodel.r_work(), fmodel.r_free()
-          
+
 def run():
   unique_codes = []
   for f in os.listdir("01/"):
@@ -159,7 +167,7 @@ def run():
       rw_3, rf_3 = get_r(pdb_file=pdb_3, mtz_file=hkl)
     else:
       rw_2, rf_2 = 0, 0
-      rw_3, rf_3 = 0, 0   
+      rw_3, rf_3 = 0, 0
     #
     ms_0 = get_model_stat(file_name = pdb_0)
     ms_1 = get_model_stat(file_name = pdb_1)
@@ -188,20 +196,20 @@ def run():
     fmt="%6.4f %6.4f %6.3f %6.2f %6.2f %5.2f %6.2f %4d %5.2f"
     s0 = fmt%(
       rw_0, rf_0,
-      ms_0.b_mean, 
-      ms_0.a_mean, 
-      ms_0.clsc, 
-      ms_0.ramachandran_outliers, 
-      ms_0.rotamer_outliers, 
+      ms_0.b_mean,
+      ms_0.a_mean,
+      ms_0.clsc,
+      ms_0.ramachandran_outliers,
+      ms_0.rotamer_outliers,
       ms_0.c_beta_dev,
       ms_0.mpscore)
     s1 = fmt%(
       rw_1, rf_1,
-      ms_1.b_mean, 
-      ms_1.a_mean, 
-      ms_1.clsc, 
-      ms_1.ramachandran_outliers, 
-      ms_1.rotamer_outliers, 
+      ms_1.b_mean,
+      ms_1.a_mean,
+      ms_1.clsc,
+      ms_1.ramachandran_outliers,
+      ms_1.rotamer_outliers,
       ms_1.c_beta_dev,
       ms_1.mpscore)
     s2 = fmt%(
@@ -223,8 +231,8 @@ def run():
       ms_3.c_beta_dev,
       ms_3.mpscore)
     print "%s %5d"%(code, ms_0.n_atoms), "|", s0, "|", s1, "|", s2, "|", s3
-    sys.stdout.flush() 
-    
-   
+    sys.stdout.flush()
+
+
 if (__name__ == "__main__"):
   run()
